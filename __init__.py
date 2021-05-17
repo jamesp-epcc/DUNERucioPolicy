3332aa748baeccb85087d3bfb7d62196a5fb2748
James Perry <j.perry@epcc.ed.ac.uk>
2021-05-17 14:49:56 +0100

    Add function to return lfn2pfn algorithms

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
-from .path_gen import register_dune_surl_algorithm
-from .lfn2pfn import register_dune_lfn2pfn_algorithm
+#from .path_gen import register_dune_surl_algorithm
+from .lfn2pfn import lfn2pfn_DUNE
 
 SUPPORTED_VERSION="1.20.7"
 
-register_dune_surl_algorithm()
-register_dune_lfn2pfn_algorithm()
+def get_lfn2pfn_algorithms():
+    return { 'DUNE': lfn2pfn_DUNE }
+
+#register_dune_surl_algorithm()
+#register_dune_lfn2pfn_algorithm()
