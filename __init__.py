c39d39e06e06d54553f0aaa6e9a54420fab2bb77
James Perry <j.perry@epcc.ed.ac.uk>
2022-01-19 09:59:58 +0000

    Change algorithm registration in line with Rucio PR #5118

--- __init__.py
+++ __init__.py
@@ -3,8 +3,5 @@ from .lfn2pfn import lfn2pfn_DUNE
 
 SUPPORTED_VERSION="1.20.7"
 
-def get_lfn2pfn_algorithms():
-    return { 'DUNE': lfn2pfn_DUNE }
-
-def get_surl_algorithms():
-    return { 'DUNE': construct_surl_dune }
+def get_algorithms():
+    return { 'lfn2pfn': { 'DUNE': lfn2pfn_DUNE }, 'surl': { 'DUNE': construct_surl_dune } }
